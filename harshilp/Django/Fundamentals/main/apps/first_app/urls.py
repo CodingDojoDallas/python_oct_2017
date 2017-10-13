bd9a1b93cfce41924ac0a3063feca0eb8c4807d4
Harshil Patel <h.gibbs62@gmail.com>
2017-10-13 13:30:47 -0500

    oct 13th starting django

---
+++ harshilp/Django/Fundamentals/main/apps/first_app/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+urlpatterns = [
+    url(r'^$', views.index),    # This line has changed!
+    url(r'^test$', views.test)     # This line has changed!
+]
